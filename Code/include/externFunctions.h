adedba244425e4b847e7349b22346d07b9b78c3c
Shane Wood <townlake101@gmail.com>
2023-04-18 08:03:25 -0400

    Added new functions

--- Code/include/externFunctions.h
+++ Code/include/externFunctions.h
@@ -7,3 +7,6 @@ float raddec(float rad); //radians to degress
 float loc(float a, float b, float c); // law of cosines, returns angle c in degress 
 float pythermhypt(float sidea, float sidec); //returns side b
 float decrad(float deg); //degrees to radians
+
+void walk(int &Cycle, ramp &FB, ramp &Height, float time, float backDistance, float upDistance);
+void turning(int &Cycle, ramp &LR, ramp &Height, float timee, float LRDistance, float upDistance);
